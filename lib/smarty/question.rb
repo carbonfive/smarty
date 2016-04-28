bcbfc581209b7eeb764bfbb02d08f2e7279758bb
Michael Wynholds <mike@carbonfive.com>
2016-04-28 13:30:29 -0700

    Better mock data

--- lib/smarty/question.rb
+++ lib/smarty/question.rb
@@ -9,7 +9,7 @@ module Smarty
     end
 
     def self.search(text)
-      [ Question.new("slack://somewhere", "---") ]
+      [ Question.new(link: "https://carbonfive.slack.com/archives/codeo/p1461863849000033", text: "What is the Codeo?") ]
     end
 
     def save
